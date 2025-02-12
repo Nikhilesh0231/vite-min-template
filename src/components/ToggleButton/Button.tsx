2513851b3b602b7136b21f37e71cb46233dbc7b9
Nikhilesh Tiwari <tnikhilesh0231@gmail.com>
2025-02-12 23:00:44 +0000

    new commit

--- src/components/ToggleButton/Button.tsx
+++ src/components/ToggleButton/Button.tsx
@@ -1,9 +1,19 @@
 import './Button.css'
-const Button = ({toggle,changehandler,setToggle}) => {
+interface ButtonProps {
+  toggle: boolean;         // Determines if the button is toggled or not
+  changehandler: () => void; // Function to change the toggle state
+  setToggle: React.Dispatch<React.SetStateAction<boolean>>; // Function to set the toggle state
+}
 
+const Button: React.FC<ButtonProps> = ({ toggle, changehandler, setToggle }) => {
+  // Handler to toggle the state (when clicked)
+  const handleClick = () => {
+    changehandler();
+    setToggle(!toggle); // Toggle the state
+  };
 return (
     <div className={(toggle)? "button" :"button1"}>
-      <div onClick={()=>{changehandler()}} className={(toggle) ? 'toggle' : 'toggle1'}></div>
+      <div onClick={()=>{handleClick()}} className={(toggle) ? 'toggle' : 'toggle1'}></div>
     </div>
   );
 };
